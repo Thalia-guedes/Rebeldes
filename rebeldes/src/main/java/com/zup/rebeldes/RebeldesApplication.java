416d1f4096f0e95c7d684cbc1d84a1f1472fa0f1
Thalia-guedes <thalia.guedes@zup.com.br>
2023-09-27 11:33:11 -0300

    subindo a main

---
+++ rebeldes/src/main/java/com/zup/rebeldes/RebeldesApplication.java
@@ -0,0 +1,13 @@
+package com.zup.rebeldes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebeldesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RebeldesApplication.class, args);
+	}
+
+}
