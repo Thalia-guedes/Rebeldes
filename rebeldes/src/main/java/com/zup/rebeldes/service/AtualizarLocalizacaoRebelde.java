ac9b96ce47c2c1e15960c27d8bbce4b333367fd9
Ana.Vitoria.Nunes <ana.nunes@zup.com.br>
2023-09-28 16:19:49 -0300

    subindo atualização

---
+++ rebeldes/src/main/java/com/zup/rebeldes/service/AtualizarLocalizacaoRebelde.java
@@ -0,0 +1,6 @@
+package com.zup.rebeldes.service;
+
+import com.zup.rebeldes.model.Localizacao;
+
+public record AtualizarLocalizacaoRebelde(Long id, String nome_rebelde, Localizacao localizacao_rebelde) {
+}
